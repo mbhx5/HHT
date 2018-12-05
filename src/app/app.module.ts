af0951be3087ee9a1dc12a92746631c77d83b4ec
mbhx5 <mbhx5@gmail.com>
2018-12-05 02:06:24 +0100

    home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AuthService } from '../providers/auth-service';
 import { SplitPane } from '../providers/split-pane';
 import { Common } from '../providers/common';
 import { HttpModule } from "@angular/http";
+import { HttpClientModule } from "@angular/common/http";
+
 import { Welcome } from '../pages/welcome/welcome';
 import { Login } from '../pages/login/login';
 import { Signup } from '../pages/signup/signup';
@@ -20,6 +22,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { MomentModule } from 'angular2-moment';
 import { LinkyModule } from 'angular-linky';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Toast } from '@ionic-native/toast';
+import { DataServiceProvider } from '../providers/data-service/data-service';
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -32,7 +39,7 @@ import { LinkyModule } from 'angular-linky';
     TabsPage
   ],
   imports: [
-    BrowserModule,HttpModule,MomentModule,LinkyModule,
+    BrowserModule,HttpModule,MomentModule,LinkyModule,HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -48,8 +55,9 @@ import { LinkyModule } from 'angular-linky';
   ],
   providers: [
     StatusBar,
-    SplashScreen,AuthService,SplitPane,Common,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    SplashScreen,AuthService,SplitPane,Common,BarcodeScanner,Toast,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DataServiceProvider
   ]
 })
 export class AppModule {}
